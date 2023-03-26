e5b07e24c62e560c2c3f466d126f9e9ce2310cb1
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-03-26 10:00:48 -0300

    Refatorando file and fileUpload

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -1,10 +1,8 @@
 <?php
 namespace App\Http\Controllers;
-
 use App\Http\Requests\FileRequest;
 use App\Models\File;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class FilesController extends Controller
 {
@@ -32,39 +30,19 @@ class FilesController extends Controller
 
     //Registrar novo documento.
     public function create(FileRequest $request){
-        $file = $request->file('files');
-        $data['title'] = $request->title;
-        $data['name'] = $file->getClientOriginalName();
-        $data['path'] = $file->store(session('email'));
-        $data['user_id'] = session('id_user');
-        $file = File::create($data);
-        if($file){
-            return redirect()->back()->with('success',"Documento salvo com sucesso.");
-        }
-        return redirect()->back()->with('error','Falha ao registrar documento.');
+        return File::fileCreate($request->except(['_token']), session('id_user'),session('email'));
     }
 
     public function download($id){
-        return File::download($id);
+        return File::fileDownload($id);
     }
 
     //Método que vai atualizar status de PENDENTE para ACEITO ou REJEITADO
     public function updateStatus(int $id, Request $request){
-        $file = File::where('id',$id)
-        ->update(['status'=>$request->status,'analist_name'=>$request->analist_name]);
-        if($file){
-            return redirect()->back()->with('success','Atualizado com sucesso.');
-        }
-        return redirect()->back()->with('error','Falha ao atualizar registro.');
+        return File::fileUpdateStatus($id,$request->except(['_token','_method']));
     }
 
     public function delete($id){
-        $file = File::find($id);
-        if($file){
-            Storage::delete($file->path);
-            $file->delete($id);
-            return redirect()->back()->with('success','Registro excluido com sucesso.');
-        }
-        return redirect()->back()->with('error', 'Falha ao remover registro.');
+        return File::fileDelete($id);
     }
 }
